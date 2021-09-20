cbf273420abb27d756009ff49e2e24e34a8d616a
Siraj <sgsdbs@gmail.com>
2021-09-20 20:08:40 +0300

    Реализовал базовые crud операции (6 section)

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,37 @@
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+
+
+interface Props {
+    activities: Activity[];
+    submitting: boolean;
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id?: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+}
+
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity,
+    editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList submitting={submitting} activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                    <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm} />}
+                {editMode &&
+                    <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
+                }
+            </Grid.Column>
+        </Grid>
+    )
+}
