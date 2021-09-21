664128ac6e02984a89aa3526b7d221f3bcfeebaf
Siraj <sgsdbs@gmail.com>
2021-09-21 21:47:22 +0300

    Добавил стейт менеджер mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,20 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+
+  const { activityStore } = useStore();
+
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -17,17 +23,9 @@ function App() {
 
   useEffect(() => {
 
+    activityStore.loadActivities();
 
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+  }, [activityStore])
 
 
   function handleSelectActivity(id: string) {
@@ -79,14 +77,16 @@ function App() {
 
   }
 
-  if (loading) return <LoadingComponent content="Loading app" />
+  if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />
 
   return (
     <Fragment>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
+
+
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
@@ -107,4 +107,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
