f2e614bc20e4e06b47dd097e9bf18e4006f7e055
Siraj <sgsdbs@gmail.com>
2021-09-23 20:37:46 +0300

    Реализовал удаление через mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -13,27 +11,12 @@ function App() {
   const { activityStore } = useStore();
 
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [submitting, setSubmitting] = useState(false);
-
   useEffect(() => {
 
     activityStore.loadActivities();
 
   }, [activityStore])
 
-
-
-
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)]);
-      setSubmitting(false);
-    })
-
-  }
 
   if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />
 
@@ -42,17 +25,8 @@ function App() {
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
 
-
-        <ActivityDashboard
-          activities={activityStore.activities}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-
-        />
+        <ActivityDashboard />
       </Container>
-
-
-
 
     </Fragment>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,26 +1,20 @@
 import { observer } from "mobx-react-lite";
 import { Grid } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 
-interface Props {
-    activities: Activity[];
-    submitting: boolean;
-    deleteActivity: (id: string) => void;
-}
 
-export default observer(function ActivityDashboard({ activities, deleteActivity, submitting }: Props) {
+export default observer(function ActivityDashboard() {
 
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList submitting={submitting} activities={activities} deleteActivity={deleteActivity} />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
